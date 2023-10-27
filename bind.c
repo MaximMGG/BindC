f4fd933d3005dc4cb1b4af63eecb287117b96263
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-10-27 22:37:53 +0300

    add help menu method and switch for first flag

--- bind.c
+++ bind.c
@@ -1,9 +1,14 @@
 #include "util.h"
 
 
+
+void ShowHelp();
+void InitUser();
+
 int main() {
 
     puts("Hello let's start or enter help for show some referencies");
+    InitUser();
    
     char *user_enter;
     gets_s(user_enter, 100);
@@ -12,12 +17,46 @@ int main() {
 
         char **user_command = parsUserEnter(user_enter, strlen(user_enter));
 
-        //TODO (Maxim) think about switch statement for user_command array
+        if (strcmp(user_command[0], "show")) {
+            ShowHelp();
+        }
 
+        //TODO (Maxim) think about switch statement for user_command array
+        switch (user_command[0][0]) {
+            case 't':
+                break;
+            case 'a':
+                break;
+            case 'f':
+                break;
+            case 'b':
+                break;
+            case 'c':
+                break;
+        }
+      
 
         gets_s(user_enter, 100);
     }
 
-
     return 0;
 }
+
+
+void ShowHelp() {
+    puts("t word -> go to google tranlator and translate \"word\"");
+    puts("a word - translation -> add to current file word and translation");
+    puts("f:");
+    puts("\tf s file_name -> set current file");
+    puts("\tf c file_name -> create new file");
+    puts("\tf d full_path_to_dir -> set full path to directory with files");
+    puts("\tf show -> show all file in directory");
+    puts("b:");
+    puts("\tb cp name value -> create bind with name (name) and http(s) (value)");
+    puts("\tb cc parent_name name value -> create child of bind (parent_name) with name (name) and http(s) (value)");
+    puts("\tb dp name -> delete bind with name (name)");
+    puts("\tb dc parent_name name -> create child of bind (parent_name) with name (name)");
+    puts("\tb show -> show all binds");
+    puts("c:");
+}
+
