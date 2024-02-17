468d4fd5bd6b0eab4d77b99532444e275c8b7fc3
maximhrunenko <maxhrunenko@gmail.com>
2024-02-17 17:13:35 +0200

    write main functional

--- stable/headers/bind.h
+++ stable/headers/bind.h
@@ -11,7 +11,7 @@ typedef struct {
 typedef struct {
     str *bind_name;
     str *bind_value;
-    List *bind_children;
+    C_BIND **bind_children;
     unsigned int children_count;
 } BIND;
 
@@ -23,20 +23,29 @@ typedef struct {
 typedef enum {
     BIND_OK,
     BIND_CANT_PARS_USER_NAME,
+    BIND_CANT_OPEN_CONFIG_FILE,
+    BIND_CANT_FIND_BIND_WITH_NAME,
 }BIND_STATUS;
 
 BIND *Bind_create_bind(str *name, str *value);
 BIND_STATUS Bind_add_parent(BIND *b, BIND_APP *app);
 BIND_STATUS Bind_add_child(BIND *b, str *name, str *value);
 BIND_STATUS Bind_remove_child(BIND *b, str *name);
-BIND_STATUS Bind_delete_bind(BIND *b, BIND_APP *app);
+BIND_STATUS Bind_delete_bind(str *b, BIND_APP *app);
+
 void Bind_show_help();
 void Bind_show_binds(BIND_APP *app);
+
+str *Bind_get_bindval(str *name, BIND_APP *app);
+str *Bind_get_bindchildval(str *pname, str *cname, BIND_APP *app);
+
 
 BIND_APP *Bind_load_binds_from_disk();
 BIND_STATUS Bind_save_binds_on_disk(BIND_APP *app);
 
 char *Bind_get_error();
+
+void Bind_destroy_app();
 
 
 #endif //_BIND_H_
--- stable/src/bind.c
+++ stable/src/bind.c
@@ -5,11 +5,95 @@
 #define null NULL
 
 static const char *PATH_TO_CONF = "/home/%s/.local/share/bindhelper/resources/bindconf.cfg";
+static char *full_path;
 BIND_STATUS bind_status;
 BIND_APP *app;
 
-static str *get_pathtoconf() {
+static void Bind_free_child(C_BIND *b) {
+    str_free(b->bind_name);
+    str_free(b->bind_value);
+    free(b);
+    b = null;
+}
 
+static void Bind_free_bind(BIND *b) {
+    if (b->children_count > 0) {
+        for (int i = 0; i < b->children_count; i++) {
+            Bind_free_child(b->bind_children[i]);
+        }
+        free(b->bind_children);
+        b->children_count = 0;
+    }
+    str_free(b->bind_name);
+    str_free(b->bind_value);
+    free(b);
+    b = null;
+}
+
+
+BIND *Bind_create_bind(str *name, str *value) {
+    BIND *new = malloc(sizeof(BIND));
+    new->bind_name = str_copy(name);
+    new->bind_value = str_copy(value);
+    new->children_count = 0;
+    new->bind_children = NULL;
+
+    return new;
+}
+
+
+BIND_STATUS Bind_add_parent(BIND *b, BIND_APP *app) {
+    if (app->bind_count == 0) {
+        app->binds = (BIND **) malloc(sizeof(BIND *));
+        app->binds[0] = b;
+    } else {
+        app->binds = (BIND **) realloc(app->binds, sizeof(BIND *) * app->bind_count + 1);
+        app->binds[app->bind_count++] = b;
+    }
+    return BIND_OK;
+}
+
+
+BIND_STATUS Bind_add_child(BIND *b, str *name, str *value) {
+    if (b->children_count == 0) {
+        b->bind_children = (C_BIND **) malloc(sizeof(C_BIND *));
+    } else {
+        b->bind_children = (C_BIND **) realloc(b->bind_children, sizeof(C_BIND *) * b->children_count + 1);
+    }
+    C_BIND *new_child = (C_BIND *) malloc(sizeof(C_BIND));
+    new_child->bind_name = str_copy(name);
+    new_child->bind_value = str_copy(value);
+    b->bind_children[b->children_count++] = new_child;
+
+    return BIND_OK;
+}
+
+BIND_STATUS Bind_remove_child(BIND *b, str *name) {
+    for(int i = 0; i < b->children_count; i++) {
+        if (strcmp(b->bind_children[i]->bind_name->str, name->str) == 0) {
+            Bind_free_child(b->bind_children[i]);
+            if (i != b->children_count) {
+                for(int j = i; j < b->children_count - 1; j++) {
+                    b->bind_children[j] = b->bind_children[j + 1];
+                }
+                b->children_count--;
+                break;
+            }
+        } 
+    }
+    return BIND_OK;
+}
+
+BIND_STATUS Bind_delete_bind(str *b, BIND_APP *app) {
+    for(int i = 0; i < app->bind_count; i++) {
+        if (strcmp(b->str, app->binds[i]->bind_name->str) == 0) {
+            Bind_free_bind(app->binds[i]);
+        }
+    }
+    return BIND_OK;
+}
+
+static str *get_pathtoconf() {
     char *name;
     name = getlogin();
     if (name == NULL) {
@@ -18,6 +102,8 @@ static str *get_pathtoconf() {
     char buf[200];
     snprintf(buf, 200, PATH_TO_CONF, name);
     str *path = STR(buf, path);
+    full_path = malloc(path->len);
+    strcpy(full_path, path->str);
 
     return path;
 }
@@ -84,12 +170,46 @@ BIND_APP *Bind_load_binds_from_disk() {
 }
 
 BIND_STATUS Bind_save_binds_on_disk(BIND_APP *app) {
-
+    FILE *f = fopen(full_path, "r");
+    if (f == null) {
+        return BIND_CANT_OPEN_CONFIG_FILE;
+    }
     
-
-
-
+    for(int i = 0; i < app->bind_count; i++) {
+        fputs("-p", f);
+        fputs(app->binds[i]->bind_name->str, f);
+        fputs(app->binds[i]->bind_value->str, f);
+        for(int j = 0; j < app->binds[i]->children_count; j++) {
+            fputs("-c", f); 
+            fputs(app->binds[i]->bind_children[j]->bind_name->str, f);
+            fputs(app->binds[i]->bind_children[j]->bind_value->str, f);
+        }
+    }
     return BIND_OK;
+}
+
+str *Bind_get_bindval(str *name, BIND_APP *app) {
+    for(int i = 0; i < app->bind_count; i++) {
+        if (strcmp(name->str, app->binds[i]->bind_name->str) == 0) {
+            return app->binds[i]->bind_value;
+        }
+    }
+    return null;
+}
+
+
+str *Bind_get_bindchildval(str *pname, str *cname, BIND_APP *app) {
+    for(int i = 0; i < app->bind_count; i++) {
+        if (strcmp(pname->str, app->binds[i]->bind_name->str) == 0) {
+            BIND *b = app->binds[i];
+            for (int j = 0; j < b->children_count; j++) {
+                if (strcmp(cname->str, b->bind_children[j]->bind_name->str) == 0) {
+                    return b->bind_children[j]->bind_value;
+                } 
+            }
+        }
+    }
+    return null;
 }
 
 char *Bind_get_error() {
@@ -100,6 +220,20 @@ char *Bind_get_error() {
         case BIND_OK: {
             return "Status OK";
         }
+        case BIND_CANT_OPEN_CONFIG_FILE: {
+            return "Can't open config file, maybe path incorrent";
+        }
+        case BIND_CANT_FIND_BIND_WITH_NAME: {
+            return "Can't find bind with than name";
+        }
     }
     bind_status = BIND_OK;
+}
+
+void Bind_destroy_app() {
+    for(int i = 0; i < app->bind_count; i++) {
+        Bind_free_bind(app->binds[i]);
+    }
+    free(app->binds);
+    free(app);
 }
